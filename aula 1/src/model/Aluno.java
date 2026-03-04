3a32e5f0d4d2cdc024697399e364ade150f57f95
Bruno <brunophgjf@gmail.com>
2026-03-04 15:51:57 -0300

    Ok

--- aula 1/src/model/Aluno.java
+++ aula 1/src/model/Aluno.java
@@ -1,6 +1,8 @@
 package model;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 public class Aluno extends Pessoa {
 
@@ -9,12 +11,31 @@ public class Aluno extends Pessoa {
     private int semestreInicio;
 
 
+    private Situacao situacao;
+    private List<Diario> diarios = new ArrayList<>();
+
+    public List<Diario> getDiarios() {
+        return diarios;
+    }
+
+    public void setDiarios(List<Diario> diarios) {
+        this.diarios = diarios;
+    }
+
     public Aluno() {
     }
 
     public Aluno(String cpf, Date dataNascimento, String email, String endereco, long id, String identidade, String nome, String telefone, int anoInicio) {
         super(cpf, dataNascimento, email, endereco, id, identidade, nome, telefone);
         this.anoInicio = anoInicio;
+    }
+
+    public Situacao getSituacao() {
+        return situacao;
+    }
+
+    public void setSituacao(Situacao situacao) {
+        this.situacao = situacao;
     }
 
     public int getAnoInicio() {
